58598cf3db1b12f888a854f6e2fabd0e1c786b3c
Marcos Nunes <marcosnunesmbs@gmail.com>
2024-05-16 22:22:01 -0300

    create new home

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/read', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'read.html'));
+});
+
 app.get('/movie', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'movie.html'));
 });
